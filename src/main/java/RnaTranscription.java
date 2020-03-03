54b1705d492e22bfa896e941aa1a79a81f60d3ce
Aaron Contreras <aaronlcaq@gmail.com>
2020-03-03 16:01:31 -0500

    Modified to use a HashMap

--- src/main/java/RnaTranscription.java
+++ src/main/java/RnaTranscription.java
@@ -1,6 +1,21 @@
-import java.util.stream.Collectors;
+import java.util.HashMap;
 public class RnaTranscription {
-    public static void main(String[] args) {
-        System.out.println("u".chars().map(c -> c = (char) c).collect(Collectors.joining()));
+
+    public String transcribe(String dna) {
+        if (dna.isEmpty()) return dna;
+
+        HashMap<Character, Character> rna = new HashMap<Character, Character>();
+        
+        rna.put('C', 'G');
+        rna.put('G', 'C');
+        rna.put('T', 'A');
+        rna.put('A', 'U');
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < dna.length(); i++) {
+            builder.append(rna.get(dna.charAt(i)));
+        }
+        return builder.toString();
     }
+
 }
